517c8c07e335b81694e44bfbbbb7761c1c1041de
jiabin <1123565780@qq.com>
2023-06-18 21:11:28 +0800

    chore: update eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,3 @@
-//  {
-//   "root": true,
-//   "extends": [
-//     "@antfu",
-//     // 'plugin:vue/vue3-essential',
-//     // 'eslint:recommended',
-//     "@vue/eslint-config-typescript",
-//     "@vue/eslint-config-prettier/skip-formatting"
-//   ],
-//   "rules":{
-//     "no-console": 0
-//   },
-//   "overrides": [
-//     {
-//       "files": [
-//         "cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"
-//       ],
-//       "extends": [
-//         "plugin:cypress/recommended"
-//       ]
-//     }
-//   ],
-//   "parserOptions": {
-//     "ecmaVersion": "latest"
-//   }
-// }
-
-
 module.exports =  {
   root: true,
   extends: [
