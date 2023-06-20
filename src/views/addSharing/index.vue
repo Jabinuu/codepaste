220e4824ad373ce9f15d8081eccab72c50617aed
jiabin <1123565780@qq.com>
2023-06-20 10:07:21 +0800

    refactor: update dir structure

--- src/views/addSharing/index.vue
+++ src/views/addSharing/index.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import ContentInput from '@/views/addSharing/components/ContentInput.vue'
-import PasteSettings from '@/views/addSharing/components/PasteSettings.vue'
+import ContentInput from '@/views/addSharing/components/contentInput/ContentInput.vue'
+import PasteSettings from '@/views/addSharing/components/pasteSettings/PasteSettings.vue'
 </script>
 
 <template>
