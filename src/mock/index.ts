fb38dd861e5e7498cd3b1b8efbf2046ddddc752b
jiabin <1123565780@qq.com>
2023-06-22 23:34:01 +0800

    feat: codeInput font style

--- src/mock/index.ts
+++ src/mock/index.ts
@@ -4,4 +4,5 @@ import codesInfo from '@/mock/modules/codeContent'
 // Mock.mock('/mock/getCodeInfo', codesData)
 Mock.mock('/mock/uploadCode', 'post', (options) => {
   codesInfo.push(JSON.parse(options.body))
+  console.log(codesInfo)
 })
--- src/views/addSharing/components/contentInput/ContentInput.vue
+++ src/views/addSharing/components/contentInput/ContentInput.vue
@@ -4,9 +4,16 @@ import { content, onBlurTextArea } from './hook'
 
 <template>
   <h3>新增代码段</h3>
-  <a-textarea v-model:value="content" placeholder="输入你的内容..." :rows="16" class="mb-30" @blur="onBlurTextArea" />
+  <a-textarea v-model:value="content" placeholder="输入你的内容..." class="mb-30" @blur="onBlurTextArea" />
 </template>
 
 <style lang="less" scoped>
-
+  textarea.ant-input{
+    min-height: 398px;
+    border-radius: 3px;
+    font-family: Consolas,Menlo,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,monospace,serif;
+    font-size: 13px;
+    tab-size: 4;
+    background-color: #F5F5F5;
+  }
 </style>
