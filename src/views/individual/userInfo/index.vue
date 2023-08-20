824ee8510ed4e5e43f8969ac4cc3ce395fe4c2d8
jiabin <1123565780@qq.com>
2023-08-21 01:22:04 +0800

    feat: change info form

--- src/views/individual/userInfo/index.vue
+++ src/views/individual/userInfo/index.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+import descriptions from './descriptions.vue'
+import infoForm from './infoForm.vue'
 
+const tabs = [descriptions, infoForm]
+const currentTab = ref(0)
+
+function onChange(e: any) {
+  currentTab.value = e
+}
 </script>
 
 <template>
@@ -22,36 +31,9 @@
         </a-col>
       </a-row>
     </a-card>
-    <a-card class="bottom-card" title="个人信息">
-      <template #extra>
-        <a href="#">编辑信息</a>
-      </template>
-      <a-descriptions :column="1">
-        <a-descriptions-item label="简介">
-          暂无
-        </a-descriptions-item>
-        <a-descriptions-item label="电话号码">
-          1810000000
-        </a-descriptions-item>
-        <a-descriptions-item label="兴趣">
-          编程、羽毛球、健身
-        </a-descriptions-item>
-        <a-descriptions-item label="现地址">
-          河南省 郑州市
-        </a-descriptions-item>
-        <a-descriptions-item label="职业">
-          <a-tag color="blue">
-            在校生
-          </a-tag>
-        </a-descriptions-item>
-        <a-descriptions-item label="邮箱地址">
-          1123565780@qq.com
-        </a-descriptions-item>
-        <a-descriptions-item label="注册时间">
-          2023-06-19 09:22
-        </a-descriptions-item>
-      </a-descriptions>
-    </a-card>
+    <keep-alive>
+      <component :is="tabs[currentTab]" class="bottom-card" @change="onChange" />
+    </keep-alive>
   </div>
 </template>
 
---
+++ src/views/individual/userInfo/infoForm.vue
@@ -0,0 +1,79 @@
+<script lang="ts" setup>
+import { reactive } from 'vue'
+import type { UnwrapRef } from 'vue'
+
+interface FormState {
+  introdction: string
+  tel: string
+  hobby: string
+  location: string
+  job: string
+  mail: string
+}
+defineEmits(['change'])
+const formState: UnwrapRef<FormState> = reactive({
+  introdction: '',
+  tel: '',
+  hobby: '',
+  location: '',
+  job: '',
+  mail: '',
+})
+const formItemLayout = {
+  labelCol: { span: 2 },
+  wrapperCol: { span: 8 },
+}
+const buttonItemLayout = {
+  wrapperCol: { span: 14, offset: 2 },
+}
+</script>
+
+<template>
+  <div class="form-container">
+    <a-form layout="horizontal" :model="formState" v-bind="formItemLayout">
+      <a-form-item label="简介">
+        <a-input v-model:value="formState.introdction" placeholder="输入简介" />
+      </a-form-item>
+      <a-form-item label="电话号码">
+        <a-input v-model:value="formState.tel" placeholder="输入电话号码" />
+      </a-form-item>
+      <a-form-item label="兴趣">
+        <a-input v-model:value="formState.hobby" placeholder="输入兴趣" />
+      </a-form-item>
+      <a-form-item label="现地址">
+        <a-input v-model:value="formState.location" placeholder="输入现地址" />
+      </a-form-item>
+      <a-form-item label="职业">
+        <a-input v-model:value="formState.job" placeholder="输入职业" />
+      </a-form-item>
+      <a-form-item label="邮箱地址">
+        <a-input v-model:value="formState.mail" placeholder="输入邮箱地址" />
+      </a-form-item>
+      <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
+        <a-button type="primary" class="submit-button" @click="$emit('change', 0)">
+          确认
+        </a-button>
+        <a-button @click="$emit('change', 0)">
+          返回
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<style>
+.form-container {
+  background-color: #fff;
+  padding: 40px 0 24px;
+  .ant-input{
+    border-radius: 6px;
+  }
+  .submit-button {
+    margin-right: 20px;
+  }
+
+  .ant-btn {
+    border-radius: 6px;
+  }
+}
+</style>
