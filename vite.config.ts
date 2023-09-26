715d3a5bd83a74efe93b71fd25b41bab5f5d5a0b
jiabin <1123565780@qq.com>
2023-09-26 22:34:26 +0800

    feat: update vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
@@ -24,7 +23,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': './src',
     },
   },
 })
